8026247cfa0e279bc3ecb929993752ee72c8d78e
Kostas Andreadis <kandread@umass.edu>
2019-09-23 18:08:42 -0400

    Construct river topology from routing table

--- confluence/integrator.py
+++ confluence/integrator.py
@@ -7,16 +7,33 @@ Algorithms for integrating SWOT-estimated discharge.
 from functools import partial
 from scipy.optimize import minimize
 import numpy as np
+import pandas as pd
 
 
 class MeanFlow:
     """Mean flow discharge integrator."""
 
-    def __init__(self, n, A0, H, W, S, A, dA, downstream):
+    def __init__(self, n, A0, H, W, S, A, dA, routing_table):
         self.nreaches = len(A0)
         self.A0 = np.array(A0)
         self.n = np.array(n)
         self.data = (H, W, S, A, dA)
+        self.rivs = self._riverTopology(routing_table)
+
+    def _riverTopology(self, routing_table):
+        """Create river topology from routing table. River topology is
+        a dictionary with each entry being a list of upstream reaches for
+        the specific reach."""
+        rout = pd.read_csv(routing_table)
+        rout = rout.dropna(axis=0)[['GridID', 'HydroID', 'NextDownID']]
+        rivs = {k: [] for k in rout['GridID']}
+        hyd = {row[1]['HydroID']: row[1]['GridID'] for row in rout.iterrows()}
+        for row in rout.iterrows():
+            if row[1]['NextDownID'] > 0:
+                rivs[hyd[row[1]['NextDownID']]].append(hyd[row[1]['HydroID']])
+        idx = {k: i for i, k in enumerate(rivs)}
+        rivs = {i: [idx[l] for l in rivs[k]] for i, k in enumerate(rivs)}
+        return rivs
 
     def objective(self, x):
         """Objective function for mean-flow integration."""
