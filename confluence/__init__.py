e550a43f9ee1b38bda6f41050491c71f9d54b02f
Kostas Andreadis <kandread@umass.edu>
2019-09-23 18:57:17 -0400

    Fix removal of flagged data

--- confluence/__init__.py
+++ confluence/__init__.py
@@ -10,10 +10,12 @@ import numpy as np
 
 def removeFlagged(H, W, S):
     """Clean up data."""
-    i = np.where(np.all(H > -1000), axis=1)
+    i = np.where(np.all(H > -1000, axis=1))
     H = H[i, :]
-    i = np.where(np.all(W > -1000), axis=1)
+    i = np.where(np.all(W > -1000, axis=1))
     W = W[i, :]
-    i = np.where(np.all(S > -1000), axis=1)
+    i = np.where(np.all(S > -1000, axis=1))
     S = S[i, :]
     return H, W, S
+
+
